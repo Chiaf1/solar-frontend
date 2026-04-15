a17bd190f0fc8c10f1b80871e90d30962950aeab
Daniele <28843028+Chiaf1@users.noreply.github.com>
2026-04-15 17:24:00 +0200

    created first example with the layout definition
    Created the first example with the layout definition

--- cmd/solar-frontend/main.go
+++ cmd/solar-frontend/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.LoadHTMLGlob("web/templates/*.html")
+	r.Static("/static", "./web/static")
+
+	r.GET("/", func(ctx *gin.Context) {
+		now := time.Now()
+
+		dayName := now.Weekday().String()
+		date := now.Format("02 january 2006")
+		timerStr := now.Format("15:04:05")
+
+		ctx.HTML(200, "base.html", gin.H{
+			"DayName":  dayName,
+			"FullDate": date,
+			"Time":     timerStr,
+		})
+	})
+
+	r.Run(":8080")
+}
