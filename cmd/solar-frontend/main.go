975bcead3701cfecab3d1ffcabb1f6b2b66207bd
Daniele <28843028+Chiaf1@users.noreply.github.com>
2026-04-16 15:26:35 +0200

    chart data updated from backend with fixed values

--- cmd/solar-frontend/main.go
+++ cmd/solar-frontend/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"html/template"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type ChartData struct {
+	Labels []string  `json:"labels"`
+	Values []float64 `json:"values"`
+}
+
 func main() {
+
+	data := ChartData{
+		Labels: []string{
+			"00:00", "02:00", "04:00", "06:00",
+			"08:00", "10:00", "12:00", "14:00",
+			"16:00", "18:00", "20:00", "22:00",
+		},
+		Values: []float64{
+			0, 0, 0, 0.5,
+			1.2, 2.8, 4.1, 3.9,
+			2.6, 1.1, 0.3, 0,
+		},
+	}
+	jsonData, _ := json.Marshal(data)
 
 	r := gin.Default()
 
@@ -16,6 +37,10 @@ func main() {
 	r.GET("/", func(ctx *gin.Context) {
 		now := time.Now()
 		ctx.HTML(200, "index.html", gin.H{
+			// Dati per grafico
+			"ChartTodayJSON": template.JS(jsonData),
+
+			// Data e ora
 			"DayName":  now.Weekday().String(),
 			"FullDate": now.Format("02 January 2006"),
 			"Time":     now.Format("15:04"),
--- web/static/js/charts.js
+++ web/static/js/charts.js
@@ -1,28 +1,17 @@
 const canvasToday = document.getElementById("chart-today");
+const rawData = document.getElementById("chart-today-data");
 
-if (!canvasToday) {
-    console.warn("Canvas chart-today non trovato");
-} else {
-    const labels = [
-        "00:00", "02:00", "04:00", "06:00",
-        "08:00", "10:00", "12:00", "14:00",
-        "16:00", "18:00", "20:00", "22:00",
-    ];
-
-    const productionData = [
-        0, 0, 0, 0.5,
-        1.2, 2.8, 4.1, 3.9,
-        2.6, 1.1, 0.3, 0,
-    ];
+if (canvasToday && rawData) {
+    const chartData = JSON.parse(rawData.textContent);
 
     new Chart(canvasToday, {
         type: "line",
         data: {
-            labels: labels,
+            labels: chartData.labels,
             datasets: [
                 {
                     label: "Produzione oggi",
-                    data: productionData,
+                    data: chartData.values,
                     borderColor: "#f4b400",
                     backgroundColor: "rgba(244, 180, 0, 0.2)",
                     tension: 0.3
